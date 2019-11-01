ecda0853dd586dedea78ac740ab5badf2100daa2
Stephen Tung <stephen_tung@executivecentre.com>
2019-11-01 17:27:27 +0800

    Moved program to CLI project and created a LIB project for other helper utils

---
+++ GetEventStoreUtil.Lib/EventStoreHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GetEventStoreUtil
+{
+    public static class EventStoreHelper
+    {
+        public static Uri CreateConnectionUri(string host, int port, string username, string password)
+        {
+            //return new Uri("tcp://admin:changeit@localhost:1113");
+            return new Uri($"tcp://{username}:{password}@{host}:{port}");
+        }
+    }
+}
